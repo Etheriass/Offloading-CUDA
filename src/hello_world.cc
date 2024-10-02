0412d48422bedf5c55a8a463822bb805d8aec5e6
Etheriass <funckialexjoly@gmail.com>
2024-10-02 15:50:24 -0400

    Setup CMake (test not working)

---
+++ src/hello_world.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "hello_world.h"
+
+void hello_world() {
+    std::cout << "Hello world" << std::endl;
+}
+
+int main() {
+    hello_world();
+    return 0;
+}
---
+++ test/hello_world_test.cc
@@ -0,0 +1,6 @@
+#include "hello_world.h"
+
+int main() {
+    hello_world();  // Call the function
+    return 0;
+}
