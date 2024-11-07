5e4e30a1a672ca06ef96ebb23e34fd48a22f6abf
Etheriass <funckialexjoly@gmail.com>
2024-11-07 19:30:46 +0000

    Solve pxtas error

--- src_C20/off_vector.cuh
+++ src_C20/off_vector.cuh
@@ -4,15 +4,16 @@
 #include <string>
 #include <atomic>
 
-class OffVector {
+class OffVector
+{
 private:
-    float* d_data;
-    size_t size; // real size of the vector
-    size_t* d_size; // pointer to size of the vector on device
-    size_t* h_size; // pointer to size of the vector on host
+    float *d_data;
+    size_t size;    // real size of the vector
+    size_t *d_size; // pointer to size of the vector on device
+    size_t *h_size; // pointer to size of the vector on host
     std::string filename;
-    bool* d_need_load; // flag to indicate if data needs to be loaded (on device)
-    bool* h_need_load; // flag to indicate if data needs to be loaded (on host)
+    bool *d_need_load; // flag to indicate if data needs to be loaded (on device)
+    bool *h_need_load; // flag to indicate if data needs to be loaded (on host)
 
     __host__ void load_chunk(size_t start, size_t chunk_size);
 
@@ -20,7 +21,16 @@ public:
     OffVector(size_t N, std::string filename);
     ~OffVector();
 
-    __device__ __host__ float operator[](size_t i) const;
+    __device__ __host__ float& operator[](size_t i)
+    {
+        if (i >= *d_size)
+        {
+            *d_need_load = true;
+            while (i >= *d_size)
+            {} // Wait for data to be loaded
+        }
+        return d_data[i];
+    }
     __host__ void check_and_load();
 };
 
